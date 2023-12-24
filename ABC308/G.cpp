65f3942b9674a07378fa025909d04059d07797aa
spin-spin-ryo <19991217r@gmail.com>
2023-12-24 13:40:00 +0900

    solver G

--- ABC308/G.cpp
+++ ABC308/G.cpp
@@ -18,7 +18,7 @@ using namespace std;
 using ll = long long;
 #define mod 998244353
 #define INF 1e+16
-#define rep(i, x) for(int i = 0; i < (x); i++)
+#define rep(i, x) for(ll i = 0; i < (x); i++)
 
 using  vector2d = vector<vector<int> >;
 
@@ -30,28 +30,90 @@ void print(vector<T> v){
     return;
 }
 
+
 int main(){
-    int N,M;cin >> N >> M;
-    vector<string> eat(N);
-    rep(i,N) cin >> eat[i];
-    vector<string> name(M);
-    vector<int> price(M);
-    rep(i,M) cin >> name[i];
-    int p0; cin >> p0;
-    rep(i,M) cin >> price[i];
-    int sum = 0;
-    rep(i,N){
-        bool done = false;
-        rep(j,M){
-            if (name[j] == eat[i]){
-                sum += price[j];
-                done = true;
-                continue;
+    ll Q; cin >> Q;
+    multiset<ll> s;
+    multiset<ll> min_set;
+    while(Q>0){
+        Q--;
+        int q; cin >> q;
+        if (q == 1){
+            ll x; cin >> x;
+            s.insert(x);
+            if (s.size() > 1){
+                auto itr = s.lower_bound(x);
+                auto itr_last = s.end();
+                itr_last --;
+                bool flag1 = false;
+                bool flag2 = false;
+                if (itr != s.begin()){
+                    itr -- ;
+                    ll y = (*itr);
+                    ll z = y^x;
+                    itr ++;
+                    min_set.insert(z);
+                    flag1 = true;
+                }
+
+                if (itr != itr_last & itr != s.end()){
+                    itr ++ ;
+                    ll y = (*itr);
+                    ll z = y^x;
+                    itr --;
+                    min_set.insert(z);
+                    flag2 = true;
+                }
+                if (flag1 & flag2){
+                    itr ++ ;
+                    ll y1 = (*itr);
+                    itr--;
+                    itr--;
+                    ll y2 = (*itr);
+                    min_set.erase(min_set.find(y1^y2));
+                    itr++;
+                }
             }
         }
-        if (!done){
-            sum+= p0;
+        if (q == 2){
+            ll x; cin >> x;
+            auto itr = s.lower_bound(x);
+            auto itr_last = s.end();
+            itr_last --;
+            bool flag1 = false;
+            bool flag2 = false;
+            if (itr != s.begin()){
+                itr -- ;
+                ll y = (*itr);
+                ll z = y^x;
+                itr ++;
+                min_set.erase(min_set.find(z));
+                flag1 = true;
+            }
+
+            if (itr != itr_last & itr != s.end()){
+                itr ++ ;
+                ll y = (*itr);
+                ll z = y^x;
+                itr --;
+                min_set.erase(min_set.find(z));
+                flag2 = true;
+            }
+
+            if (flag1 & flag2){
+                itr ++ ;
+                ll y1 = (*itr);
+                itr--;
+                itr--;
+                ll y2 = (*itr);
+                min_set.insert(y1^y2);
+                itr++;
+            }
+            s.erase(itr);
+        }
+        if (q == 3){
+            auto itr = min_set.begin();
+            cout << (*itr) << endl;
         }
     }
-    cout << sum << endl;
 }
